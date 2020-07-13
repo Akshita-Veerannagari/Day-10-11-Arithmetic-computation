67b5999d7ab5306d48f1cafcdc52a134eb71e26a
Akshita-Veerannagari <akshitareddy.v@gmail.com>
2020-07-13 21:55:54 +0530

    Added Computation of a+b*c

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -4,3 +4,6 @@ read -p "Enter the first number: " a
 read -p "Enter the second number: " b
 read -p "Enter the third number: " c
 
+result_one=$(( a+b*c ))
+echo "The result of a+b*c is $result_one"
+
