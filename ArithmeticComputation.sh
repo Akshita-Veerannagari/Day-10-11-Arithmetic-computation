fe5cb0740eafd04c3876668a6caea0daf7da423b
Akshita-Veerannagari <akshitareddy.v@gmail.com>
2020-07-13 22:11:34 +0530

    Results stored in dictionary after every computation

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -5,11 +5,17 @@ read -p "Enter the second number: " b
 read -p "Enter the third number: " c
 
 result_one=$(( a+b*c ))
-echo "The result of a+b*c is $result_one"
 result_two=$(( a*b+c ))
-echo "The result of a*b+c is $result_two"
 result_three=`echo $a $b $c | awk '{print $3+($1/$2)}'`
-echo "The result of c+a/b is $result_three"
 result_four=`echo $a $b $c | awk '{print ($1%$2)+$3}'`
-echo "The result of a%b+c is $result_four"
 
+declare -A operations
+operations["a+b*c"]=$result_one
+operations["a*b+c"]=$result_two
+operations["c+a/b"]=$result_three
+operations["a%b+c"]=$result_four
+
+for index in ${!operations[@]}
+do
+	echo $index" : "${operations[$index]}
+done
