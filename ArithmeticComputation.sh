d1dbb6d4c492d189e78bee48d5e36c34dbd2cf86
Akshita-Veerannagari <akshitareddy.v@gmail.com>
2020-07-13 22:20:31 +0530

    Array sorted in descending order

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,5 +1,24 @@
 #! /bin/bash
 
+function sort()
+{
+        arr=("$@")
+        n=${#arr[@]}
+        for ((i = 0; i<n; i++))
+        do
+                for((j = 0; j<n-i-1; j++))
+                do
+                        if [ ${arr[j]} -lt ${arr[$((j+1))]} ]
+                        then
+                                temp=${arr[j]}
+                                arr[$j]=${arr[$((j+1))]}
+                                arr[$((j+1))]=$temp
+                        fi
+                done
+        done
+        echo "Array in descending order is:"${arr[@]}
+}
+
 read -p "Enter the first number: " a
 read -p "Enter the second number: " b
 read -p "Enter the third number: " c
@@ -21,4 +40,4 @@ do
 	array[((counter++))]=${operations[$index]}
 done
 
-echo ${array[@]}
+sort ${array[@]}
